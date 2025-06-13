f86b61badedc806376f5c999c5ad3328434aff46
anandu-kk <109016442+anandu-kk@users.noreply.github.com>
2025-06-13 17:39:57 +0530

    Refactor signal docstring for clarity; add requirements file with dependencies

--- vault/signals.py
+++ vault/signals.py
@@ -5,9 +5,7 @@ import os
 
 @receiver(post_delete, sender=EncryptedFile)
 def delete_encrypted_file_on_delete(sender, instance, **kwargs):
-    """
-    Deletes the associated file from the filesystem when an EncryptedFile instance is deleted.
-    """
+    # Deletes the associated file from the filesystem when an EncryptedFile instance is deleted.
     if instance.file:
         if os.path.isfile(instance.file.path):
             os.remove(instance.file.path)
